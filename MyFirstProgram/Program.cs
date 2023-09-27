338b7c1d05902bdc7210dbd5cae199aec78cd77c
andritox55 <andresww.55@hotmail.com>
2023-09-27 13:15:19 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Design;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int number = 10;
+
+            // check if number is bigger than 10 if not print else statement
+            if (number > 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            } else
+            {
+                Console.WriteLine("Oj. Lågt tal!");
+            }
+
+
+            // takes input from user and print name
+            Console.WriteLine("Vad heter du?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hej {name}!");
+
+
+            // print every number from 0 to a chosen number
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
